3f1b9f33f07fb5efd7c322e82787b89844525e82
Kellen Hart <kellenhart@gmail.com>
2016-12-20 01:59:22 +0000

    fix exec permission; install make

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ apt-get update
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 apt-get update
-apt-get install -y docker-engine
+apt-get install -y docker-engine make
