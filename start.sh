cfe6917240ebdcd932481cc0ba0c1d29fd5b50e1
Kellen Hart <kellenhart@gmail.com>
2016-12-20 01:52:23 +0000

    finish start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
-#  This script should start up your service on port 8080 w
-# This starts the app in development mode.  Make a config to start it with more production-like settings
+#!/bin/sh
+# TODO: This starts the app in development mode.  Make a config to start it with more production-like settings
+
+make build && make run
